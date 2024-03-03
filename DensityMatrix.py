96721c3f84933ee71abdd4e142f4c24ef6469a06
Manvi Agrawal <manvimeerut@gmail.com>
2024-03-03 00:47:13 -0800

    Fix wrong C integrals

--- DensityMatrix.py
+++ DensityMatrix.py
@@ -9,29 +9,31 @@ class DensityMatrix():
     
     def __init__(self) -> None:
         self._rho = qt.tensor(I, I)
+        (self._a,self._b,self._c,self._d) = np.diag(np.array(self._rho))
+
 
     def __init__(self, rho: qt.Qobj) -> None:
         self._rho = rho
-        (self._a,self._b,self._c,self._d) = np.diag(np.array(self._rho))
+        (self._a,self._b,self._c,self._d) = np.diag(np.array(rho))
     
     @property
     def rho(self):
         return self._rho
-
+    
     def h_spectrum(self, wh=6.46, J=215.0):
         jh = J/62.37 # Divide by larmor freq of H to get ppm scale
         print("H spectrum")
-        print(f"Peak1 at {np.around(wh-jh/2,4)} ppm with integral={np.round(self._a-self._c, 4)}")
-        print(f"Peak2 at {np.around(wh+jh/2,4)} ppm with integral={np.round(self._b-self._d, 4)}")
+        print(f"Peak1 at {np.around(wh-jh/2, 2)} ppm with integral={np.round(self._a-self._c, 2)}")
+        print(f"Peak2 at {np.around(wh+jh/2, 2)} ppm with integral={np.round(self._b-self._d, 2)}")
 
     def c_spectrum(self, wc=73.331, J=215):
         jc = J/15.68 # Divide by larmor freq of C to get ppm scale
         print("C spectrum")
-        print(f"Peak1 @ {np.around(wc-jc/2,4)} ppm with integral={np.round(self._a-self._b)}")
-        print(f"Peak2 @ {np.around(wc+jc/2,4)} ppm with integral={np.round(self._c-self._d)}")
+        print(f"Peak1 @ {np.around(wc-jc/2, 2)} ppm with integral={np.round(self._a-self._b, 2)}")
+        print(f"Peak2 @ {np.around(wc+jc/2, 2)} ppm with integral={np.round(self._c-self._d, 2)}")
 
 def main():
-    rho = qt.Qobj(np.diag([1, 0.6, -0.6, -1]), dims=([[2, 2], [2, 2]]))
+    rho = qt.Qobj(np.diag([1, 0.6, -1, -0.6]), dims=([[2, 2], [2, 2]]))
     d1 = DensityMatrix(rho)
     print("helo..")
     d1.h_spectrum()
