b76bb7a967ad8dda628dc03cf59ce60e799907b0
Manvi Agrawal <manvimeerut@gmail.com>
2024-03-03 13:47:47 -0800

    Improvements to run algorithm

---
+++ Algorithm.py
@@ -0,0 +1,190 @@
+
+from PulseSequence import PulseSequence
+
+from Gate import *
+import matplotlib.pyplot as plt
+
+class Algorithm():
+    def __init__(self, name):
+        self._name = name
+
+        self._p0 = PulseSequence("P0")
+
+        self._p1 = PulseSequence("P1")\
+        .add(Pulse("Sx(+pi/2)", Sx, +np.pi/2))\
+        .add(Pulse("2IzSz(aka delay(1/2J))", IzSz, +np.pi))\
+        .add(Pulse("Sy(+pi/2)", Sy, +np.pi/2))\
+        .add(Pulse("Ix(+pi/2)", Ix, +np.pi/2))\
+        .add(Pulse("2IzSz(aka delay(1/2J))", IzSz, +np.pi))\
+        .add(Pulse("Iy(+pi/2)", Iy, +np.pi/2))\
+        
+        self._p2 = PulseSequence("P2")\
+        .add(Pulse("Ix(+pi/2)", Ix, +np.pi/2))\
+        .add(Pulse("2IzSz(aka delay(1/2J))", IzSz, +np.pi))\
+        .add(Pulse("Sx(+pi/2)", Sx, +np.pi/2))\
+        .add(Pulse("Iy(+pi/2)", Iy, +np.pi/2))\
+        .add(Pulse("2IzSz(aka delay(1/2J))", IzSz, +np.pi))\
+        .add(Pulse("Sy(+pi/2)", Sy, +np.pi/2))\
+        
+        self._seq = PulseSequence(self._name)
+
+    def state_prep(self, state):
+        if state=="00":
+            self._p0.add(X_i).add(X_s)
+            self._p1.add(X_i).add(X_s)
+            self._p2.add(X_i).add(X_s)
+            return self
+
+        elif state=="01":
+            self._p0.add(X_i)
+            self._p1.add(X_i)
+            self._p2.add(X_i)
+            return self
+
+        elif state=="10":
+            self._p0.add(X_s)
+            self._p1.add(X_s)
+            self._p2.add(X_s)
+            return self
+
+
+        elif state=="11":
+            self._p0 = self._p0
+            self._p1 = self._p0
+            self._p2 = self._p0
+            return self
+
+        else:
+            raise ValueError(f"Incorrect state : {state}")
+        
+    def add_sequence(self, algo_seq):
+        self._p0.add_seq(algo_seq)
+        self._p1.add_seq(algo_seq)
+        self._p2.add_seq(algo_seq)
+        return self
+
+    def add_pulse(self, algo_pulse):
+        self._p0.add(algo_pulse)
+        self._p1.add(algo_pulse)
+        self._p2.add(algo_pulse)
+        return self
+
+    
+    def execute_algorithm(self):
+        # fig = plt.figure(figsize=(9, 9))
+        # # grid for pairs of subplots
+        # grid = plt.GridSpec(2, 1)
+
+        # h_plot = fig.add_subplot(grid[0])
+        # # '\n' is important
+        # h_plot.set_title(f'Hydrogen\n', fontweight='semibold', size=14)
+        # h_plot.set_axis_off()
+
+        # gs_h = gridspec.GridSpecFromSubplotSpec(1, 3, subplot_spec=grid[0])
+
+        # # real subplot #1
+        # ax = fig.add_subplot(gs_h[0])
+        # ax.set_title(f'P0-1H')
+
+        # # real subplot #2
+        # ax = fig.add_subplot(gs_h[1], sharey=ax)
+        # ax.set_title(f'P1-1H')
+        
+        # # real subplot #3
+        # ax = fig.add_subplot(gs_h[2], sharey=ax)
+        # ax.set_title(f'P2-1H')
+
+        self._p0.print_sequence()
+        print("\n")
+        self._p1.print_sequence()
+        print("\n")
+        self._p2.print_sequence()
+
+
+        fig, axs = plt.subplots(2, 3, sharey="row", figsize=(10,8))
+        # fig = plt.figure(figsize=(8, 6))
+
+        p0_sim = self._p0.compile().evolve_pho_th()
+
+
+        p0_sim.plot_h_spectrum(axs[0,0], param_dict={})
+        p0_sim.plot_c_spectrum(axs[1,0], param_dict={})
+        axs[0,0].set_title("P0-1H")
+        axs[1,0].set_title("P0-13C")
+
+
+        p1_sim = self._p1.compile().evolve_pho_th()
+        p1_sim.plot_h_spectrum(axs[0,1], param_dict={"label":"hi"})
+        p1_sim.plot_c_spectrum(axs[1,1], param_dict={})
+        axs[0,1].set_title("P1-1H")
+        axs[1,1].set_title("P1-13C")
+
+
+        p2_sim = self._p2.compile().evolve_pho_th()
+        p2_sim.plot_h_spectrum(axs[0,2], param_dict={})
+        p2_sim.plot_c_spectrum(axs[1,2], param_dict={})
+        axs[0,2].set_title("P2-1H")
+        axs[1,2].set_title("P2-13C")
+
+        pho_net = (p0_sim.rho+p1_sim.rho+p2_sim.rho)/3
+
+        p0_h_pk1 = p0_sim.h_pk1()
+        p1_h_pk1 = p1_sim.h_pk1()
+        p2_h_pk1 = p2_sim.h_pk1()
+        h_pk1 = (p0_h_pk1 + p1_h_pk1 + p2_h_pk1)/3
+        h_pk1 = np.round(h_pk1, 3)
+
+        p0_h_pk2 = p0_sim.h_pk2()
+        p1_h_pk2 = p1_sim.h_pk2()
+        p2_h_pk2 = p2_sim.h_pk2()
+        h_pk2 = (p0_h_pk2 + p1_h_pk2 + p2_h_pk2)/3
+        h_pk2 = np.round(h_pk2, 3)
+
+
+        p0_c_pk1 = p0_sim.c_pk1()
+        p1_c_pk1 = p1_sim.c_pk1()
+        p2_c_pk1 = p2_sim.c_pk1()
+        c_pk1 = (p0_c_pk1 + p1_c_pk1 + p2_c_pk1)/3
+        c_pk1 = np.round(c_pk1, 3)
+
+
+        p0_c_pk2 = p0_sim.c_pk2()
+        p1_c_pk2 = p1_sim.c_pk2()
+        p2_c_pk2 = p2_sim.c_pk2()
+        c_pk2 = (p0_c_pk2 + p1_c_pk2 + p2_c_pk2)/3
+        c_pk2 = np.round(c_pk2, 3)
+
+
+        print("Resultant Matrix::")
+        print(pho_net)
+
+        h_spectrum = f"H net spectrum: {h_pk1}, {h_pk2}"
+        c_spectrum = f"C net spectrum: {c_pk1}, {c_pk2}"
+
+        print(h_spectrum)
+        print(c_spectrum)
+
+
+
+        fig.suptitle(f"{self._name} \n {h_spectrum} \n {c_spectrum}")
+        # axs[0]
+        plt.legend()
+        plt.show()
+
+        
+    
+
+def main():
+    print("start")
+    ta = Algorithm("Tempoaral Averaging -- |00>")
+    ta.state_prep("00")
+   
+
+    ta.execute_algorithm()
+    print("\nend")
+
+
+
+if __name__=="__main__":
+    main()
+
--- DensityMatrix.py
+++ DensityMatrix.py
@@ -4,6 +4,14 @@ from __future__ import annotations
 
 import qutip as qt
 import numpy as np
+import matplotlib.pyplot as plt
+
+def lorentian( w, w0,alpha, tau):
+    #     n = 1 + 0.5*s + 4*(delta)*(delta)/(gamma*gamma)
+        n = alpha*tau
+        d = (1j)*(w-w0) + tau
+        y = n/d
+        return np.real(y)
 
 class DensityMatrix():
     
@@ -15,7 +23,8 @@ class DensityMatrix():
     def __init__(self, rho: qt.Qobj) -> None:
         self._rho = rho
         (self._a,self._b,self._c,self._d) = np.diag(np.array(rho))
-    
+      
+
     @property
     def rho(self):
         return self._rho
@@ -26,18 +35,76 @@ class DensityMatrix():
         print(f"Peak1 at {np.around(wh-jh/2, 2)} ppm with integral={np.round(self._a-self._c, 2)}")
         print(f"Peak2 at {np.around(wh+jh/2, 2)} ppm with integral={np.round(self._b-self._d, 2)}")
 
+    def h_pk1(self):
+        return np.round(self._a-self._c, 2)
+
+    def h_pk2(self):
+        return np.round(self._b-self._d, 2)
+
+        
+    def plot_h_spectrum(self, ax, param_dict=None, wh=6.46, J=215.0, tau=0.016):
+        jh = J/62.37 # Divide by larmor freq of H to get ppm scale
+        
+
+        w = np.linspace(0, 10, 1000)
+        w0_pk1 = np.around(wh-jh/2, 2)
+        alpha_pk1 = np.round(self._a-self._c, 2)
+        y1= lorentian(w, w0_pk1, alpha_pk1, tau)
+
+        w0_pk2 = np.around(wh+jh/2, 2)
+        alpha_pk2 = np.round(self._b-self._d, 2)
+        y2= lorentian(w, w0_pk2, alpha_pk2, tau)
+
+        y = y1+y2
+
+        plot = ax.plot(w, y, label=f"a1={alpha_pk1} \n a2={alpha_pk2}")
+        ax.legend()
+        return plot
+        # plt.show()
+
+
     def c_spectrum(self, wc=73.331, J=215):
         jc = J/15.68 # Divide by larmor freq of C to get ppm scale
         print("C spectrum")
         print(f"Peak1 @ {np.around(wc-jc/2, 2)} ppm with integral={np.round(self._a-self._b, 2)}")
         print(f"Peak2 @ {np.around(wc+jc/2, 2)} ppm with integral={np.round(self._c-self._d, 2)}")
 
+    def c_pk1(self):
+        return np.round(self._a-self._b, 2)
+
+    def c_pk2(self):
+        return np.round(self._c-self._d, 2)
+
+    def plot_c_spectrum(self, ax, param_dict=None, wc=73.331, J=215.0, tau=0.016):
+        jc = J/15.68 # Divide by larmor freq of C to get ppm scale
+        
+        # print("C spectrum")
+
+        w = np.linspace(60, 90, 1000)
+        w0_pk1 = np.around(wc-jc/2, 2)
+        alpha_pk1 = np.round(self._a-self._b, 2)
+        y1= lorentian(w, w0_pk1, alpha_pk1, tau)
+
+        w0_pk2 = np.around(wc+jc/2, 2)
+        alpha_pk2 = np.round(self._c-self._d, 2)
+        y2= lorentian(w, w0_pk2, alpha_pk2, tau)
+
+        y = y1+y2
+
+        plot = ax.plot(w, y, label=f"a1={alpha_pk1}\n a2={alpha_pk2}")
+        ax.legend()
+        return plot
+        # plt.show()
+
 def main():
     rho = qt.Qobj(np.diag([1, 0.6, -1, -0.6]), dims=([[2, 2], [2, 2]]))
     d1 = DensityMatrix(rho)
     print("helo..")
-    d1.h_spectrum()
-    d1.c_spectrum()
+    fig, ax = plt.subplots(1, 1)
+    d1.plot_h_spectrum(ax, param_dict={})
+    plt.show()
+    # d1.h_spectrum()
+    # d1.c_spectrum()
 
 if __name__=="__main__": 
     main()
